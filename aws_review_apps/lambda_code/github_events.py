0702b7cda57dd880730783ab59fa1753a1523c00
marianobrc <marianomartinez84@gmail.com>
2021-11-23 17:09:06 -0300

    Fix a parsing issue in lambda function

--- aws_review_apps/lambda_code/github_events.py
+++ aws_review_apps/lambda_code/github_events.py
@@ -1,6 +1,7 @@
 """
 Lambda function code used to create a CodeBuild project which deploys the CDK pipeline stack for the branch.
 """
+import json
 import logging
 import os
 
@@ -10,8 +11,9 @@ logger.setLevel(logging.DEBUG)
 
 def handler(event, context):
     """Lambda function handler"""
-    logger.debug(f"GH Event received:\n{event['body']}")
-    gh_branch = event["body"].get("ref")
+    gh_event = json.loads(event['body'])
+    logger.debug(f"GH Event received:\n{gh_event}")
+    gh_branch = gh_event.get("ref")
     logger.info(f"GH event detected in branch:\n{gh_branch}")
     return {
       "statusCode": "200",
