3a46bd61175bc1e848ed5d819522754c0b2ff42a
marianobrc <marianomartinez84@gmail.com>
2021-11-24 19:21:35 -0300

    Make lambda to create a a codebuild project to build pipelines for review apps

--- aws_review_apps/lambda_code/github_events.py
+++ aws_review_apps/lambda_code/github_events.py
@@ -1,12 +1,34 @@
 """
-Lambda function code used to create a CodeBuild project which deploys the CDK pipeline stack for the branch.
+Lambda function used to create a CodeBuild project which deploys a new pipeline stack for every new PR.
 """
 import json
 import logging
 import os
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+
+
+def generate_build_spec(branch: str, account_id: str, region: str):
+    """Generates the build spec file used for the CodeBuild project"""
+    return f"""version: 0.2
+env:
+  variables:
+    BRANCH: {branch}
+    ACCOUNT_ID: {account_id}
+    REGION: {region}
+phases:
+  pre_build:
+    commands:
+      - npm install -g aws-cdk && pip install -r requirements.txt
+  build:
+    commands:
+      - cdk synth
+      - cdk deploy ReviewAppPipeline --require-approval=never
+artifacts:
+  files:
+    - '**/*'"""
 
 
 def handler(event, context):
@@ -17,6 +39,46 @@ def handler(event, context):
     src_branch = gh_event["pull_request"]["head"]["ref"]
     action = gh_event.get("action")
     logger.info(f"PR detected: {dst_branch} <- {src_branch} [{action}]")
+    if action == "opened":  # New PR
+        logger.info(f"Making a codebuild project to deploy pipeline for branch {src_branch}..")
+        repo_url = event['head']['repo']['clone_url']
+        region = os.environ['AWS_REGION']
+        account_id = os.environ['ACCOUNT_ID']
+        role_arn = os.environ['CODE_BUILD_ROLE_ARN']
+        artifact_bucket_name = os.environ['ARTIFACT_BUCKET']
+        codebuild_client = boto3.client('codebuild')
+        codebuild_client.create_project(
+            name=f'build-review-app-{src_branch}',
+            description=f"Build project to deploy a review app pipeline for branch {src_branch}",
+            source={
+                'type': 'GITHUB',
+                'location': repo_url,
+                'buildspec': generate_build_spec(
+                    branch=src_branch,
+                    account_id=account_id,
+                    region=region
+                )
+            },
+            sourceVersion=f'refs/heads/{src_branch}',
+            artifacts={
+                'type': 'S3',
+                'location': artifact_bucket_name,
+                'path': f'{src_branch}',
+                'packaging': 'NONE',
+                'artifactIdentifier': 'BranchBuildArtifact'
+            },
+            environment={
+                'type': 'LINUX_CONTAINER',
+                'image': 'aws/codebuild/standard:4.0',
+                'computeType': 'BUILD_GENERAL1_SMALL'
+            },
+            serviceRole=role_arn
+        )
+        logger.info(f"Deploying pipeline for branch {src_branch}..")
+        codebuild_client.start_build(
+            projectName=f'CodeBuild-build-review-app-{src_branch}'
+        )
+
     return {
       "statusCode": "200",
       "headers": {},
---
+++ aws_review_apps/review_app_pipeline_builder_stack.py
@@ -0,0 +1,124 @@
+import os
+from os import path
+from aws_cdk import core as cdk
+# For consistency with other languages, `cdk` is the preferred import name for
+# the CDK's core module.  The following line also imports it as `core` for use
+# with examples from the CDK Developer's Guide, which are in the process of
+# being updated to use `cdk`.  You may delete this import if you don't need it.
+from aws_cdk import core
+from aws_cdk.aws_apigateway import LambdaRestApi
+from aws_cdk.aws_iam import Role, ServicePrincipal, ManagedPolicy, PolicyStatement
+from aws_cdk import aws_lambda
+from aws_cdk import aws_s3 as s3
+from cloudcomponents.cdk_github_webhook import GithubWebhook
+from cloudcomponents.cdk_secret_key import SecretKey
+
+
+class ReviewAppPipelineBuilderStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        account_id = cdk.Aws.ACCOUNT_ID
+        region = cdk.Aws.REGION
+        github_api_token = kwargs.pop("github_api_token")
+        github_repo_url = kwargs.pop("github_repo_url")
+        super().__init__(scope, construct_id, **kwargs)
+        # Define a lambda function to create and trigger a new pipeline on new feature branch creation
+
+        # IAM Role for the AWS Lambda function
+        lambda_role = Role(
+            self,
+            'LambdaPipelineBuilderRole',
+            assumed_by=ServicePrincipal('lambda.amazonaws.com')
+        )
+        lambda_role.add_managed_policy(
+            ManagedPolicy.from_aws_managed_policy_name("service-role/AWSLambdaBasicExecutionRole")
+        )
+        lambda_role.add_to_policy(PolicyStatement(
+            actions=[
+                'codebuild:CreateProject',
+                'codebuild:StartBuild'
+            ],
+            resources=[f'arn:aws:codebuild:{region}:{account_id}:project/*']
+        ))
+        # Artifact bucket for feature AWS CodeBuild projects
+        artifact_bucket = s3.Bucket(
+            self,
+            'BranchArtifacts',
+            encryption=s3.BucketEncryption.KMS_MANAGED,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
+
+        # AWS Lambda and AWS CodeBuild projects' IAM Roles.
+        # IAM Role for the feature branch AWS CodeBuild project.
+        code_build_role = Role(
+            self,
+            'CodeBuildExecutionRole',
+            assumed_by=ServicePrincipal('codebuild.amazonaws.com'))
+        code_build_role.add_to_policy(PolicyStatement(
+            actions=['cloudformation:DescribeStacks', 'cloudformation:DeleteStack'],
+            resources=[f'arn:aws:cloudformation:{region}:{account_id}:stack/*/*']
+        ))
+        code_build_role.add_to_policy(PolicyStatement(
+            actions=['logs:CreateLogGroup', 'logs:CreateLogStream', 'logs:PutLogEvents'],
+            resources=[
+                f'arn:aws:logs:{region}:{account_id}:log-group:/aws/codebuild/review-apps-*',
+                f'arn:aws:logs:{region}:{account_id}:log-group:/aws/codebuild/review-apps-*:*']
+        ))
+        # code_build_role.add_to_policy(PolicyStatement(
+        #     actions=['codecommit:Get*', 'codecommit:List*', 'codecommit:GitPull'],
+        #     resources=[f'arn:aws:codecommit:{region}:{account_id}:{repo_name}']
+        # ))
+        code_build_role.add_to_policy(PolicyStatement(
+            actions=['s3:DeleteObject', 's3:PutObject', 's3:GetObject', 's3:ListBucket'],
+            resources=[f'{artifact_bucket.bucket_arn}/*', f'{artifact_bucket.bucket_arn}']
+        ))
+        code_build_role.add_to_policy(PolicyStatement(
+            actions=['sts:AssumeRole'],
+            resources=[f'arn:*:iam::{account_id}:role/*'],
+            conditions={
+                "ForAnyValue:StringEquals": {
+                    "iam:ResourceTag/aws-cdk:bootstrap-role": [
+                        "image-publishing",
+                        "file-publishing",
+                        "deploy"
+                    ]
+                }
+            }
+        ))
+
+        # The lambda function
+        self.review_apps_builder_lambda = aws_lambda.Function(
+            self,
+            'LambdaPipelineBuilderOnBranchCreateStack',
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            function_name='LambdaPipelineBuilderOnBranchCreate',
+            handler='github_events.handler',
+            code=aws_lambda.Code.from_asset(path.join(path.dirname(__file__), 'lambda_code')),
+            environment={
+                "ACCOUNT_ID": account_id,
+                "CODE_BUILD_ROLE_ARN": code_build_role.role_arn,
+                "ARTIFACT_BUCKET": artifact_bucket.bucket_name,
+            },
+            role=lambda_role
+        )
+
+        # Connect GH webhooks to lambda
+        self.gh_webhook_api = LambdaRestApi(self, "github-webhook", handler=self.review_apps_builder_lambda)
+        self.gh_webhook_api.root.add_method("POST")
+
+        github_api_token = SecretKey.from_plain_text(github_api_token)
+
+        # @see https://developer.github.com/v3/activity/events/types/
+        events = [
+            "pull_request"
+        ]
+
+        GithubWebhook(
+            self, "GithubWebhook",
+            github_api_token=github_api_token,
+            github_repo_url=github_repo_url,
+            payload_url=self.gh_webhook_api.url,
+            events=events,
+            log_level="debug"
+        )
