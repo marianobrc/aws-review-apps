5bf29ea63fafd7c233bdd9c6c5c93fafbcbd46c0
marianobrc <marianomartinez84@gmail.com>
2022-01-20 10:03:51 -0300

    Add extra permissions for codebuild to use cdk templates

--- aws_review_apps/review_app_pipeline_builder_stack.py
+++ aws_review_apps/review_app_pipeline_builder_stack.py
@@ -57,7 +57,7 @@ class ReviewAppPipelineBuilderStack(cdk.Stack):
             actions=[
                 'cloudformation:DescribeStacks', 'cloudformation:DeleteStack',
                 'cloudformation:GetTemplate', 'cloudformation:CreateChangeSet',
-                'cloudformation:DescribeChangeSet',
+                'cloudformation:DescribeChangeSet', 'cloudformation:DeleteChangeSet',
             ],
             resources=[f'arn:aws:cloudformation:{region}:{account_id}:stack/*/*']
         ))
